c32845d548203e4e6c2529016ac4e7cd3d131bfb
nattashalevch <nattashalev@gmail.com>
2026-05-19 16:16:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -347,6 +347,20 @@ document.addEventListener('DOMContentLoaded', function() {
       if (typeof ym === 'function') ym(109122763, 'reachGoal', 'form_submit');
     });
   }
+  // Цель: просмотр блока историй (отзывов)
+const reviewSection = document.querySelector('.stories-slider-section');
+if (reviewSection) {
+  const observer = new IntersectionObserver((entries) => {
+    if (entries[0].isIntersecting) {
+      if (typeof ym === 'function') {
+        ym(109122763, 'reachGoal', 'scroll_to_reviews');
+        console.log('Метрика: пользователь увидел отзывы');
+      }
+      observer.disconnect();
+    }
+  });
+  observer.observe(reviewSection);
+}
 
   // ==================== ЗАПУСК ====================
   renderServices();
